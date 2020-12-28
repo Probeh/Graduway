4abbcb9b57306407fa8c08cbdd54e6b1d43de964
Yonathan Probeh <Probeh@outlook.com>
2020-12-28 20:49:17 +0200

    Code Indendation

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -1,20 +1,20 @@
-import { BrowserModule                           } from '@angular/platform-browser'             ;
-import { NgModule                                } from '@angular/core'                         ;
-import { AppRoutingModule                        } from './app-routing.module'                  ;
-import { AppComponent                            } from './app.component'                       ;
-import { EmployeeComponent                       } from './employee/employee.component'         ;
-import { TaskListComponent                       } from './task-list/task-list.component'       ;
-import { TaskDetailsComponent                    } from './task-details/task-details.component' ;
-import { HttpClientModule    , HTTP_INTERCEPTORS } from '@angular/common/http'                  ;
-import { RequestInterceptor                      } from '@services/request.interceptor'         ;
-import { NavigationComponent } from './navigation/navigation.component';
+import { BrowserModule                            } from '@angular/platform-browser'             ;
+import { NgModule                                 } from '@angular/core'                         ;
+import { AppRoutingModule                         } from './app-routing.module'                  ;
+import { AppComponent                             } from './app.component'                       ;
+import { EmployeeComponent                        } from './employee/employee.component'         ;
+import { TaskListComponent                        } from './task-list/task-list.component'       ;
+import { TaskDetailsComponent                     } from './task-details/task-details.component' ;
+import { HttpClientModule     , HTTP_INTERCEPTORS } from '@angular/common/http'                  ;
+import { RequestInterceptor                       } from '@services/request.interceptor'         ;
+import { NavigationComponent                      } from './navigation/navigation.component'     ;
 
 @NgModule({
   declarations: [
     AppComponent         ,
     EmployeeComponent    ,
     TaskListComponent    ,
-    TaskDetailsComponent,
+    TaskDetailsComponent ,
     NavigationComponent
    ],
   imports: [
